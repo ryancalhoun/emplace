e02e01da93d6cb8beb4ca23483b5cb84c9c52028
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-04-17 21:51:38 -0500

    WIP adding cmake lists parsing

--- lib/emplace/cmake.rb
+++ lib/emplace/cmake.rb
@@ -15,7 +15,7 @@ module Emplace
     end
 
     def cmake_contents
-      @cmake_contents ||= File.open(cmake_lists, 'r') {|f| f.read}
+      @lines || File.open(cmake_lists, 'r') {|f| parse_cmake_file! f} && @lines
     end
 
     def project_name
@@ -34,13 +34,77 @@ module Emplace
       !! project_name && ! library_name && ! executable_name
     end
 
+    def statements
+      cmake_contents.select {|line| line.is_a? Statement}
+    end
+
+    def to_s
+      cmake_contents.map(&:text).join
+    end
+
+    def parse_cmake_file!(io)
+      @lines = []
+      io.each_line {|line| append_line! line}
+    end
+
+    class Whitespace
+      attr_reader :text
+      def initialize(text)
+        @text = text
+      end
+      def done?
+        true
+      end
+    end
+
+    class Statement
+      attr_reader :text, :name, :arguments
+      def initialize(text)
+        @text = text
+        @arguments = []
+        if m = /^\s*(\w+)\s*\(([^)]*)\)?/.match(text)
+          @name = m[1]
+          split_args m[2]
+        end
+      end
+      def done?
+        !! /\)\s*$/m.match(text)
+      end
+      def <<(line)
+        @text += line
+        if m = /([^)]*)\)?/.match(line)
+          split_args m[1]
+        end
+      end
+      def split_args(text)
+        @arguments += text.split(/\s(?=(?:[^"]|"[^"]*")*$)|[\r\n]/).select {|a| ! a.empty?}
+      end
+    end
+
+    def get_name(name)
+      if s = find(name)
+        s.arguments.first
+      end
+    end
+
+    def find(name)
+      statements.find {|s| s.name == name}
+    end
+
+    def find_all(name)
+      statements.find_all {|s| s.name == name}
+    end
+
     private
-    def get_name(type)
-      if m = /\b#{type}\s*\(\s*(\w+)/.match(cmake_contents)
-        m[1]
+    def append_line!(line)
+      if @lines.last && ! @lines.last.done?
+        @lines.last << line
+      elsif /^\s*(#.*)?$/.match(line)
+        @lines << Whitespace.new(line)
+      else
+        @lines << Statement.new(line)
       end
     end
   end
 end
 
-require 'byebug'
--- test/emplace-cmake-test.rb
+++ test/emplace-cmake-test.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'fileutils'
+require 'stringio'
 
 require 'emplace/cmake'
 
@@ -20,46 +21,94 @@ class EmplaceCMakeTest < Test::Unit::TestCase
   end
 
   def testProjectName
-    File.open('projdir/CMakeLists.txt', 'w') {|f|
-      f.puts 'project(foo)'
-    }
-    assert_equal 'foo', Emplace::CMake.new('projdir').project_name
+    assert_equal 'foo', project(<<-END).project_name
+      project(foo)
+    END
   end
 
   def testLibraryName
-    File.open('projdir/CMakeLists.txt', 'w') {|f|
-      f.puts 'add_library('
-      f.puts '   foo'
-      f.puts '   STATIC'
-      f.puts '   ${SOURCES}'
-      f.puts ')'
-    }
-    assert_equal 'foo', Emplace::CMake.new('projdir').library_name
+    assert_equal 'foo', project(<<-END).library_name
+      add_library(
+         foo
+         STATIC
+         ${SOURCES}
+      )'
+    END
   end
   def testExecutableName
-    File.open('projdir/CMakeLists.txt', 'w') {|f|
-      f.puts 'add_executable('
-      f.puts '   foo'
-      f.puts '   ${SOURCES}'
-      f.puts ')'
-    }
-    assert_equal 'foo', Emplace::CMake.new('projdir').executable_name
+    assert_equal 'foo', project(<<-END).executable_name
+      add_executable(
+         foo
+         ${SOURCES}
+      )
+    END
   end
   def testIsRoot
-    File.open('projdir/CMakeLists.txt', 'w') {|f|
-      f.puts 'add_executable('
-      f.puts '   foo'
-      f.puts '   ${SOURCES}'
-      f.puts ')'
-    }
-    assert_false Emplace::CMake.new('projdir').is_root?
+    assert_false project(<<-END).is_root?
+      add_executable(
+         foo
+         ${SOURCES}
+      )
+    END
+    assert_true project(<<-END).is_root?
+      project(
+         foo
+      )
+    END
 
-    File.open('projdir/CMakeLists.txt', 'w') {|f|
-      f.puts 'project('
-      f.puts '   foo'
-      f.puts ')'
+  end
+
+  def testStatements
+    assert_equal %w(project include_directories link_libraries), project(<<-END).statements.map(&:name)
+      project(foo)
+
+      include_directories (
+        /usr/include
+        ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+
+      link_libraries(
+        bar thing
+      )
+    END
+  end
+
+  def testArguments
+    assert_equal %w(one "" "two"), project(<<-END).find('link_libraries').arguments
+      link_libraries (
+        one
+
+        ""
+
+        "two"
+      )
+    END
+  end
+
+  def toS
+    text = <<-END
+      # CMAKE PROJECT
+
+      project(foo)
+
+      include_directories (
+        /usr/include
+        ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+
+      link_libraries(
+        bar thing
+      )
+
+    END
+
+    assert_equal text, project(text).to_s
+  end
+
+  def project(contents)
+    Emplace::CMake.new.tap {|proj|
+      proj.parse_cmake_file! StringIO.new(contents)
     }
-    assert_true Emplace::CMake.new('projdir').is_root?
   end
 end
 
